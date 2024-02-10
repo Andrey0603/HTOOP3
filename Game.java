02ce9ac272cd9e7ad02c2a068e0733631c4c4c0b
Andrey <cliff_06@bk.ru>
2024-02-10 10:43:50 +0500

    написали код в файле Game

---
+++ Game.java
@@ -0,0 +1,7 @@
+
+public interface Game {
+    //Интерфейс взаимодействий Game (должны быть описаны сигнатуры методов start, inputValue,getGameStatus)
+    void start(Integer wordSize, Integer tryCount);
+    Answer inputValue(String value);
+    GameStatus getGameStatus();
+}
